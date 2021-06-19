7e37370578b4ed38fc9a9545111093a1c456c375
daniil savelyev <44727718+kriper0nind@users.noreply.github.com>
2021-06-19 03:55:15 +0300

    webpack tempalte

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+function App() {
+  return <div>
+    this is webpack react template
+    </div>
+}
+
+const root = document.getElementById("root");
+
+render(<App/>, root)
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const config = require("dotenv").config().parsed;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: config.MODE,
+  entry: {
+    main: path.join(__dirname, "/src/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 4000,
+    hot: true,
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:__dirname + "/src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+    new webpack.DefinePlugin({
+      MODE: JSON.stringify(config.MODE),
+      URL_WEBSOCKETS: JSON.stringify(config.URL)
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ]
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg/,
+        use: ["@svgr/webpack"]
+      }
+    ]
+  }
+}
